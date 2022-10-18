4eeae64f9d65eb9cc59b57434963465b460f4963
AyechiNour <ayechii.nourr@gmail.com>
2022-10-18 21:54:03 +0100

    blocLogout

--- my-project/components/navbar.jsx
+++ my-project/components/navbar.jsx
@@ -4,18 +4,23 @@ import { changeApparence } from '../app/slices/sideSlice';
 
 const Navbar = () => {
     const dispatch = useDispatch()
-    const [apparence, setapparence] = useState(true);
+    const [apparence, setapparence] = useState(false);
     return (
-        <div className='sticky top-0 w-screen h-16 bgPink flex flex-row items-center place-content-between'>
+        <div className='sticky top-0 w-screen h-16 bgPink flex flex-row items-center place-content-between z-40'>
             <div className='flex flex-row justify-center items-center p-4'>
                 <img onClick={() => { dispatch(changeApparence()) }} className='w-7 h-7 cursor-pointer' src="/image/navIcon.png" alt="" srcset="" />
                 <h1 className='text-white font-cookie font-semibold text-xl px-4'>App Name</h1>
             </div>
-            <div className='relative'>
-                <img className='cursor-pointer w-12 h-12 mr-4 rounded-full border border-4' src="/image/nour.jpg" alt="" srcset="" />
-                <div className='absolute h-60 w-16 bg-red-700 z-50 top-0 right-0'>
-
-                </div>
+            <div className='relative z-40'>
+                <img onClick={()=>{setapparence(!apparence)}} className='cursor-pointer w-12 h-12 mr-4 rounded-full border border-4' src="/image/nour.jpg" alt="" srcset="" />
+                {apparence&&
+                <div className='absolute w-64 bg-white z-40 top-14 right-2 border-2 border-black'>
+                    <div className='flex flex-row items-center p-2'>
+                        <img className='cursor-pointer w-12 h-12 mr-4 rounded-full border border-4' src="/image/nour.jpg" alt="" srcset="" />
+                        <h2 className='text-md font-semibold'>Ayechi Nour</h2>
+                    </div>
+                    <h3 className='text-md font-semibold flex justify-end mt-2 py-2 px-4 bg-slate-100'>Log out</h3>
+                </div>}
             </div>
         </div>
     );
